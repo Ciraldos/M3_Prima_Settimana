1ba7bcf5e6797e1d620dd3da400b6d59f2027f55
Ciraldos <andreaciraldo2@gmail.com>
2024-05-20 14:20:21 +0200

    .

--- Giorno_1/ts-argomenti/tipi-dato-variabili.ts
+++ Giorno_1/ts-argomenti/tipi-dato-variabili.ts
@@ -1,15 +1,16 @@
-let numero:number = 5;
-let stringa:string = '';
+let numeri:number = 5;
+let stringa:string = "ciao";
 let booleano:boolean = true;
 
-let union:(number|string) = '5'
+let union:(number|string) = "5"
 
-let vuota:undefined|number;//questa variabile può essere undefined o number
+let vuota:undefined|number;
 vuota = 8
 
+//let array:Array<any> = [4, true] // potrebbe chiederlo il benchmark
 
-//let array:Array<any> = [4, true] //potrebbe chiderlo il benchmark
-let array:[] = []//tipo di dato array vuoto
-let arrayNumeri:number[] = [1,2,4,5,6]
+let array:[] = [] //tipo di dato array vuoto
+let arrayNumeri:number[] = [1,2,3,4] //array numeri
+let arrayNumeriStringhe:(number|string)[] = [1,3,"ciao"]
 
-let qualcosa:any = [];//bello ma è vietato
+let qualcosa:any = [];
